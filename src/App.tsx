0079f095d7b69cb6784c15f211feb9c832e0c7f4
nicoumaine <157842474+nicoumaine@users.noreply.github.com>
2025-09-17 10:23:20 -0400

    Add red rectangles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,51 @@
 import React from "react";
 import "./App.css";
 import rug from "./assets/photos/authentic_persian_rug.jpg";
-import {Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
 function App(): React.JSX.Element {
     return (
         <div className="App">
             <h1>Goofy Goober</h1>
-            <img src={rug} alt='A meme featuring several Persian rugs with the text: "Friend, Girlfriend, Boyfriend, Best friend" (all with "end" underlined). Next to them it says "Only authentic persian rug Has no END," highlighting that unlike friendships or relationships that may end, Persian rugs are timeless.' />
+            <img
+                src={rug}
+                alt='A meme featuring several Persian rugs with the text: "Friend, Girlfriend, Boyfriend, Best friend" (all with "end" underlined). Next to them it says "Only authentic persian rug Has no END," highlighting that unlike friendships or relationships that may end, Persian rugs are timeless.'
+            />
             <ul>
-            <li>Girlfriend X</li>
-            <li>Boyfriend X</li>
-            <li>Bestfriend X</li>
-            <Button onClick={ () => { console.log("Hello world!") } }>Log Hello World</Button>
-        </ul>
+                <li>
+                    Girlfriend X
+                    <div
+                        style={{
+                            width: "50px",
+                            height: "20px",
+                            backgroundColor: "red",
+                        }}
+                    ></div>
+                </li>
+                <li>
+                    Boyfriend X
+                    <div
+                        style={{
+                            width: "50px",
+                            height: "20px",
+                            backgroundColor: "red",
+                        }}
+                    ></div>
+                </li>
+                <li>
+                    Bestfriend X
+                    <div
+                        style={{
+                            width: "50px",
+                            height: "20px",
+                            backgroundColor: "red",
+                        }}
+                    ></div>
+                </li>
+                <Button onClick={() => console.log("Hello world!")}>
+                    Log Hello World
+                </Button>
+            </ul>
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
