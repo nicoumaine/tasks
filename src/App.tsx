0698371396f1f14341213de8204fbc32ebdda33c
nicoumaine <157842474+nicoumaine@users.noreply.github.com>
2025-09-17 13:05:49 -0400

    Red rectangles a 4th attempt

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,66 @@
 import React from "react";
-import "./App.css";
 import rug from "./assets/photos/authentic_persian_rug.jpg";
-import { Container, Row, Col, Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
 function App(): React.JSX.Element {
     return (
         <div className="App">
-        <Container>
-            <Row>
-            <Col
-                div {
-                      width: 500px;
-                      height: 200px;
-                      border: 10px solid red;
-                      background-color: red;
-                    }
             <h1>Goofy Goober</h1>
-            <img src={rug} alt='A meme featuring several Persian rugs with the text: "Friend, Girlfriend, Boyfriend, Best friend" (all with "end" underlined). Next to them it says "Only authentic persian rug Has no END," highlighting that unlike friendships or relationships that may end, Persian rugs are timeless.' />
+            <img
+                src={rug} // if you moved it to public/, change to: src="/assets/photos/authentic_persian_rug.jpg"
+                alt='A meme featuring several Persian rugs with the text: "Friend, Girlfriend, Boyfriend, Best friend" (all with "end" underlined). Next to them it says "Only authentic persian rug Has no END," highlighting that unlike friendships or relationships that may end, Persian rugs are timeless.'
+            />
+
             <ul>
-            <li>Girlfriend X</li>
-            <li>Boyfriend X</li>
-            <li>Bestfriend X</li>
-            <Button onClick={ () => { console.log("Hello world!") } }>Log Hello World</Button>
-        </ul>
+                <li>
+                    Girlfriend X
+                    <div
+                        style={{
+                            width: "80px",
+                            height: "20px",
+                            backgroundColor: "red",
+                            marginTop: "5px",
+                        }}
+                    ></div>
+                </li>
+                <li>
+                    Boyfriend X
+                    <div
+                        style={{
+                            width: "80px",
+                            height: "20px",
+                            backgroundColor: "red",
+                            marginTop: "5px",
+                        }}
+                    ></div>
+                </li>
+                <li>
+                    Bestfriend X
+                    <div
+                        style={{
+                            width: "80px",
+                            height: "20px",
+                            backgroundColor: "red",
+                            marginTop: "5px",
+                        }}
+                    ></div>
+                </li>
+                <Button onClick={() => console.log("Hello world!")}>
+                    Log Hello World
+                </Button>
+            </ul>
+
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
-            </Col>
-            <Col>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
             </p>
-            div {
-                  width: 500px;
-                  height: 200px;
-                  border: 10px solid red;
-                  background-color: red;
-                }
-            </Col>
-            </Row>
-            </Container>
         </div>
     );
 }
 
 export default App;
+
 
