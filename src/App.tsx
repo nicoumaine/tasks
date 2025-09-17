3f9e103fd61b2a696c0e10ee3735b7bed8f84383
nicoumaine <157842474+nicoumaine@users.noreply.github.com>
2025-09-16 23:12:13 -0400

    Fixed file serving

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <h1>Goofy Goober</h1>
-            <img src="public/authentic_persian_rug.jpg" alt='A meme featuring several Persian rugs with the text: "Friend, Girlfriend, Boyfriend, Best friend" (all with "end" underlined). Next to them it says "Only authentic persian rug Has no END," highlighting that unlike friendships or relationships that may end, Persian rugs are timeless.' />
+            <img src="/authentic_persian_rug.jpg" alt='A meme featuring several Persian rugs with the text: "Friend, Girlfriend, Boyfriend, Best friend" (all with "end" underlined). Next to them it says "Only authentic persian rug Has no END," highlighting that unlike friendships or relationships that may end, Persian rugs are timeless.' />
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
