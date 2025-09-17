5d686b0067ea302f80aafc19236917629fd40978
nicoumaine <157842474+nicoumaine@users.noreply.github.com>
2025-09-16 22:48:54 -0400

    Going back to Commit b193b7e because something bad

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,14 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <h1>Goofy Goober</h1>
+            <img src="src/8857bb5fa95f7c41a0b9ec3ebf60514f.jpg" alt='A meme featuring several Persian rugs with the text: "Friend, Girlfriend, Boyfriend, Best friend" (all with "end" underlined). Next to them it says "Only authentic persian rug Has no END," highlighting that unlike friendships or relationships that may end, Persian rugs are timeless.' />
+            <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
             </p>
-             <img src="src/8857bb5fa95f7c41a0b9ec3ebf60514f.jpg" alt='A meme featuring several Persian rugs with the text: "Friend, Girlfriend, Boyfriend, Best friend" (all with "end" underlined). Next to them it says "Only authentic persian rug Has no END," highlighting that unlike friendships or relationships that may end, Persian rugs are timeless.' />
-        </div>
-      </div>
         </div>
     );
 }
