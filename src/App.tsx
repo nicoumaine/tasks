e15f3fdc9f44ced21abf465e4cc80d024a0163ec
nicoumaine <157842474+nicoumaine@users.noreply.github.com>
2025-09-17 11:04:07 -0400

    Fix columns

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,6 @@ function App(): React.JSX.Element {
             <Row>
             <Col>
             <h1>Goofy Goober</h1>
-            </Col>
-            <Col>
             <img src={rug} alt='A meme featuring several Persian rugs with the text: "Friend, Girlfriend, Boyfriend, Best friend" (all with "end" underlined). Next to them it says "Only authentic persian rug Has no END," highlighting that unlike friendships or relationships that may end, Persian rugs are timeless.' />
             <ul>
             <li>Girlfriend X</li>
@@ -21,6 +19,8 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
+            </Col>
+            <Col>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
