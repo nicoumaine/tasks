94668c39704f197963c9754532b1056fbc0ed810
nicoumaine <157842474+nicoumaine@users.noreply.github.com>
2025-09-17 10:59:10 -0400

    Add columns

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,12 @@ import {Button} from "react-bootstrap";
 function App(): React.JSX.Element {
     return (
         <div className="App">
+        <Container>
+            <Row>
+            <Col>
             <h1>Goofy Goober</h1>
+            </Col>
+            <Col>
             <img src={rug} alt='A meme featuring several Persian rugs with the text: "Friend, Girlfriend, Boyfriend, Best friend" (all with "end" underlined). Next to them it says "Only authentic persian rug Has no END," highlighting that unlike friendships or relationships that may end, Persian rugs are timeless.' />
             <ul>
             <li>Girlfriend X</li>
@@ -20,6 +25,9 @@ function App(): React.JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
             </p>
+            </Col>
+            </Row>
+            </Container>
         </div>
     );
 }
