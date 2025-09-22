c3320d148af609f1d806e8e84a99a42f062db41c
nicoumaine <antonia.faragodumsch@maine.edu>
2025-09-21 21:48:34 -0400

    Function allRGB

--- src/arrays.ts
+++ src/arrays.ts
@@ -68,7 +68,7 @@ export function countShortWords(words: string[]): number {
  * then return true.
  */
 export function allRGB(colors: string[]): boolean {
-    return false;
+    return colors.every((color) => ["red", "blue", "green"].includes(color));
 }
 
 /**
