bd3015882f641debc9a381d21db0e4b198316ea1
nicoumaine <antonia.faragodumsch@maine.edu>
2025-09-21 21:45:48 -0400

    Function countShortWords

--- src/arrays.ts
+++ src/arrays.ts
@@ -59,7 +59,7 @@ export const shoutIfExclaiming = (messages: string[]): string[] =>
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    return 0;
+    return words.filter((word) => word.length < 4).length;
 }
 
 /**
