7b7b3575bada547002f5e93ea172ea7657684766
nicoumaine <antonia.faragodumsch@maine.edu>
2025-09-30 20:16:11 -0400

    Wrote function mergeQuestion

--- src/objects.ts
+++ src/objects.ts
@@ -153,5 +153,14 @@ export function mergeQuestion(
     contentQuestion: Question,
     { points }: { points: number },
 ): Question {
-    return contentQuestion;
+    return {
+        id: id,
+        name: name,
+        body: contentQuestion.body,
+        type: contentQuestion.type,
+        options: [...contentQuestion.options],
+        expected: contentQuestion.expected,
+        points: points,
+        published: false,
+    };
 }
