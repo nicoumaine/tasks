4f2700daf2e063cb1254a5ef77172034e7c6eae6
nicoumaine <antonia.faragodumsch@maine.edu>
2025-10-10 19:47:41 -0400

    Write publishAll

--- src/nested.ts
+++ src/nested.ts
@@ -69,7 +69,10 @@ export function makeAnswers(questions: Question[]): Answer[] {
  * Hint: as usual, do not modify the input questions array
  */
 export function publishAll(questions: Question[]): Question[] {
-    return [];
+    return questions.map((question) => ({
+        ...question,
+        published: true,
+    }));
 }
 
 /***
