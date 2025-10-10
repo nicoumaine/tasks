38e15c8aa9c24188dcdc69da0059702eb8135285
nicoumaine <antonia.faragodumsch@maine.edu>
2025-10-10 19:44:42 -0400

    Write makeAnswers function

--- src/nested.ts
+++ src/nested.ts
@@ -55,7 +55,12 @@ export function getNames(questions: Question[]): string[] {
  * making the `text` an empty string, and using false for both `submitted` and `correct`.
  */
 export function makeAnswers(questions: Question[]): Answer[] {
-    return [];
+    return questions.map((question) => ({
+        questionId: question.id,
+        text: "",
+        submitted: false,
+        correct: false,
+    }));
 }
 
 /***
